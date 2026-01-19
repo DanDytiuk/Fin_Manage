6670a2ed93b9d8d1c51d638f6b8567538c030564
DanDytiuk <dandytiuk@gmail.com>
2026-01-19 08:19:15 +0200

    Dev limits

--- DanDytiuk/FinManage/View/Windows/LimitWindow.xaml.cs
+++ DanDytiuk/FinManage/View/Windows/LimitWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FinManage.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,12 @@ namespace FinManage.View.Windows
         public LimitWindow()
         {
             InitializeComponent();
+            var vm = new LimitWindowsViewModel
+            {
+                CloseAction = Close
+            };
+
+            DataContext = vm;
         }
     }
 }
