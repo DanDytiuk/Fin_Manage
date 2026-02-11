ed83b2bd2b239f31b6124bcdb7c0689f90e5dc74
DanDytiuk <dandytiuk@gmail.com>
2026-02-11 21:15:26 +0200

    Add Resources and add main infrastructure for control income

--- DanDytiuk/FinManage/Languages/Resources-EN.Designer.cs
+++ DanDytiuk/FinManage/Languages/Resources-EN.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace FinManage.Properties {
+namespace FinManage.Languages {
     using System;
     
     
@@ -22,14 +22,14 @@ namespace FinManage.Properties {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "18.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    public class Resources {
+    public class Resources_EN {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
         private static global::System.Globalization.CultureInfo resourceCulture;
         
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources() {
+        internal Resources_EN() {
         }
         
         /// <summary>
@@ -39,7 +39,7 @@ namespace FinManage.Properties {
         public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("FinManage.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("FinManage.Languages.Resources-EN", typeof(Resources_EN).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
